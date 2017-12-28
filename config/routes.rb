108a94a369fb955d32450dc927bde11b26b60d4c
kienphan <senior1206@gmail.com>
2017-12-28 16:35:03 +0700

    add rest example

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   post "/graphql", to: "graphql#execute"
   mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
+
+  resources :users do
+    get '/list', to: "users#list", on: :collection
+  end
 end
